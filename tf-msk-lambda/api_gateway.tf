ebad23087ecabc2842f6048d8303d1874ddfd8fb
orlando-baires <orlando.castillo@bairesdev.com>
2022-04-07 23:13:26 -0500

    Add API Gateway integration with Lambda

---
+++ tf-msk-lambda/api_gateway.tf
@@ -0,0 +1,85 @@
+###################
+# API GATEWAY
+###################
+
+resource "aws_api_gateway_rest_api" "api_gateway" {
+  name        = "ApigServerless"
+  description = "Terraform Serverless Application"
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+
+#####################
+# lAMBDA INTEGRATION
+#####################
+
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_method.proxy.resource_id
+  http_method = aws_api_gateway_method.proxy.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda_pocinvoke_arn
+}
+
+
+resource "aws_api_gateway_method" "proxy_root" {
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+  resource_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_root" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_method.proxy_root.resource_id
+  http_method = aws_api_gateway_method.proxy_root.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.example.invoke_arn
+}
+
+
+#########################
+# API GATEWAY DEPLOYMENT
+#########################
+
+resource "aws_api_gateway_deployment" "apig_deploy" {
+  depends_on = [
+    "aws_api_gateway_integration.lambda",
+    "aws_api_gateway_integration.lambda_root",
+  ]
+
+  rest_api_id = "${aws_api_gateway_rest_api.api_gateway.id}"
+  stage_name  = "prod"
+}
+
+###########################################
+# Allowing API Gateway to Access Lambda
+###########################################
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.lambda_poc.function_name}"
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  source_arn = "${aws_api_gateway_rest_api.api_gateway.execution_arn}/*/*"
+}
--- tf-msk-lambda/src/handler.js
+++ tf-msk-lambda/src/handler.js
@@ -1,1 +1,13 @@
-console.log('Hello World')
+'use strict';
+
+exports.handler = function (event, context, callback) {
+    var response = {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'text/html; charset=utf-8',
+        },
+        body: "Hello world!",
+    };
+    
+    callback(null, response);
+};
